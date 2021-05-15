adf18fc3e44830c7c3596e8ff7466782450a26de
c1910475033 <max.mehlfuehrer@stud.fh-campuswien.ac.at>
2021-05-15 11:55:05 +0200

    more files added

---
+++ popup.js
@@ -0,0 +1,10 @@
+let changeColor = document.getElementById("changeColor");
+
+/*chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+
+});*/
+
+function hallo(){
+    console.log("hallo");
+}
