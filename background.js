37ed43f7d03900322f105f00c1e82e3a6018dccd
c1910475033 <max.mehlfuehrer@stud.fh-campuswien.ac.at>
2021-05-16 11:28:36 +0200

    get request made

--- background.js
+++ background.js
@@ -1,3 +1,77 @@
+/*domain: ".kurier.at"
+expirationDate: 1621110946
+hostOnly: false
+httpOnly: false
+name: "__io_session_id"
+path: "/"
+sameSite: "unspecified"
+secure: false
+session: false
+storeId: "0"
+value: "55a705ece.c4667151a_1621109146009"*/
+/*let cookieinfo;
 chrome.cookies.onChanged.addListener(function(info) {
-    console.log("onChanged" + JSON.stringify(info));
+    if(info.cookie.domain === ".kurier.at"){  //check only for the kurier.at domain cookies
+       console.log(info);
+       cookieinfo = info.cookie;
+       chrome.scripting.executeScript({
+         target:{}
+       })
+       //const obj = JSON.parse(info.cookie);
+       //kurierCookies += kurierCookies.concat(info.cookie);
+    }
+    console.log("alle kurier cookies");
+   //console.log(kurierCookies.toString());
+  });*/
+
+
+let active_tab_id =0;
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {   //onactivated only triggers when clicking on the tab
+  chrome.tabs.get(tabId, current_tab_info => {
+    active_tab_id = tabId;
+    console.log(current_tab_info.url);
+    if (/^https:\/\/sport.orf.at/.test(current_tab_info.url)) {
+      //send message from backend to frontend
+      chrome.tabs.sendMessage(active_tab_id, {message:'you are on sport.orf.at'})
+      //chrome.tabs.sendMessage(active_tab_id, {message: cookieinfo.cookie})
+      chrome.scripting.executeScript(
+        {
+        target: {tabId: tabId},
+        files: ['popup.js'],
+       },
+        () => console.log('popup.js was executed')) //executeScript(tabnumber, file)
+    }
   });
+});
+
+//chrome.runtime.onMessage.addListener((request, sender, sendRepsonse) => {
+  //check if message = the message i should get and response
+    //chrome.tabs.sendMessage(active_tab_id, {message:'you are on www.kurier.at'})
+    //sendRepsonse({message: 'yo i got your message'});
+    //chrome.storage.local.get("password", value => {
+    //  console.log(value);
+    //})
+//})
+
+/*
+//console.log(chrome.extension.popup);
+var views = chrome.extension.getViews({
+    type: "popup"
+});
+for (var i = 0; i < views.length; i++) {
+    views[i].document.getElementById('x').innerHTML = "My Custom Value";
+}
+/*
+const user = {
+  username: 'demo-user'
+};
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  // 2. A page requested user data, respond with a copy of `user`
+  if (message === 'get-user-data') {
+    sendResponse(user);
+  }
+});*/
+
+//chrome.tabs.executeScript(null, {file: 'popup.js'}, () => console.log('i injected'));
--- popup.js
+++ popup.js
@@ -1,9 +1,71 @@
-let changeCookie = document.getElementById("changeCookies");
+console.log("from foreground");
+
+/*const first = document.createElement('button');
+first.innerText = "Set Data";
+first.id = "first";
+const second = document.createElement('button');
+second.innerText = "Shoutout to backend";
+second.id = "second";
+
+document.querySelector('body').appendChild(first);
+document.querySelector('body').appendChild(second);*/
+
+/*first.addEventListener('click', () => {
+  //chrome.storage.sync   //sync saves data accorss devices if you sign in on another machine
+  chrome.storage.local.set({"password": "123"});
+  console.log("I SET DATA");
+});
+
+second.addEventListener('click', () => {
+  //sendmessage init by frontend to backend
+  chrome.runtime.sendMessage({message: 'yo check the storage'}, res => console.log(res));
+  console.log('Isentthemessage');
+})*/
+
+
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
+  console.log(request);
+  if(request.message==="you are on sport.orf.at"){
+    console.log(request.message);
+    const Http = new XMLHttpRequest();
+    const Initiator ="main.js?v=2021050411-310:827";
+    const url='https://cookiedispenser.orf.at/oon-ds-banner-c/short/closed';
+    Http.open("GET", url);
+    
+    Http.send();
+      
+    Http.onreadystatechange = (e) => {
+          console.log("get request send to cookiedispenser.orf.at");
+          console.log(Http.responseText);
+    } 
+    const oondsads = new XMLHttpRequest();
+    const oondsadsURL='https://cookiedispenser.orf.at/oon-ds-ads/true';
+    oondsads.open("GET",oondsadsURL);
+    oondsads.send();
+
+    oondsads.onreadystatechange = (e) => {
+      console.log(oondsads.responseText);
+    }
+  }
+  
+});
+/*let changeCookie = document.getElementById("changeCookies");
 
 function change() {
     console.log("The content changed ... ");
 }
 
-changeCookie.addEventListener("click", "change");
+changeCookie.addEventListener("click", change);
 
 //add some functions for Button here
+
+chrome.runtime.sendMessage('get-user-data', (response) => {
+  // 3. Got an asynchronous response with the data from the background
+  console.log('received user data', response);
+});
+
+chrome.cookies.onChanged.addListener(function(info) {
+  chrome.extension.getBackgroundPage().console.log("onChanged" + JSON.stringify(info));
+});*/
+
